f0daa2318bd4f93bb46dcbd673e6bed7ada1c1cc
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-09-04 16:01:42 +0200

    fix:typo

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -9,7 +9,7 @@ const ProjectsCard = ({ url, img, github, title, text }) => {
         className="w-full object-cover rounded-t-lg h-64"
       />
       <dir className="capitalize p-8">
-        <h2 className="text-xl trcking-wide font-medium">{title}</h2>
+        <h2 className="text-xl tracking-wide font-medium">{title}</h2>
         <p className="mt-4 text-slate-700 leading-loose">{text}</p>
         <div className="mt-4 flex gap-x-4">
           <a href={url}>
